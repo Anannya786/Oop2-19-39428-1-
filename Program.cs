b4546e36ef2446e3be688fcf10814a552e3f0a2b
Anannya786 <77951252+Anannya786@users.noreply.github.com>
2021-04-30 16:11:47 +0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,36 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lab_task_march
+namespace labtask1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Student s1 = new Student("Tanvir", "13246",2.5f);
-            student s2 = new Student("Sabbir","13246",3.5f);
-            student s3 = new Student("Rahim","13246",3.5f);
+        static void Main(string[] args);
+    {
+            Account a1 = new Account("ABC", "145", 66000);
+            Account a2 = new Account("BCD", "146", 88000);
+            Account a3 = new Account("EFG", "147", 55000);
 
-            Department d1 = new Department("Computer Science", "CS");
-            Department d2 = new Department("Electrical Engineering", "EEE");
-
-            s1.Dept = d1;
-            d1.AddStudent(s1,s2,s3);
-            s2.Dept = d1;
-            s3.Dept = d2;
-            d2.AddStudent(s3);
-            s1.ShowDepartmentDetails();
-            s3.ShowDepartmentDetails();
-            Console.WriteLine("*********");
-            d1.Name = "Compute Science and Engineering";
-            d1.Id = "CSE";
-            s2.ShowDepartmentDetails();
-            Console.WriteLine("**********");
-            d2.printStudents();
-            s3.Name = "Abdur Rahim";
-            d2.PrintStudents();
-
-
+            a1.Withdraw(550);
+            a2.Withdraw(660);
+            a1.Transfer(a2, 3000);
+            a3.Deposit(4000);
+            a1.showAllTranscation();
+            a2.showAllTranscation();
+            a3.showAllTranscation();
+          }
         }
-    }
-}
+        }
