44da40c332b2f47cd0cd78ad617114974648c5e6
Anannya786 <77951252+Anannya786@users.noreply.github.com>
2021-05-04 23:10:05 +0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace labtask1
+namespace WindowsForm
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args);
-    {
-            Account a1 = new Account("ABC", "145", 66000);
-            Account a2 = new Account("BCD", "146", 88000);
-            Account a3 = new Account("EFG", "147", 55000);
-
-            a1.Withdraw(550);
-            a2.Withdraw(660);
-            a1.Transfer(a2, 3000);
-            a3.Deposit(4000);
-            a1.showAllTranscation();
-            a2.showAllTranscation();
-            a3.showAllTranscation();
-          }
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
-        }
+    }
+}
